13b1d3742cf859c8e804b49e4359a1c00cd8f722
sunluky@gmail.com <sunluky@gmail.com@df278220-4fff-2252-f868-6514807f2fe3>
2012-01-04 14:01:04 +0000

    To count spliced reads at each splicing site of candidate_isoforms.gtf

---
+++ candidate_orf.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+## Author: Lei Sun
+## Date: 09/12/2011
+## Email: leisuncumt@yahoo.com
+
+## Goal - To count spliced reads at each splicing site of candidate_isoforms.gtf
+## Input - ARGV[0] candidate_orf output from longorf.pl
+## Output - candidate:orf_length table
+
+open(ORF, $ARGV[0]) || die "Could not read from argv[0], program halting.";
+while(my $current_line=<ORF>){
+	chomp($current_line);
+    
+    # split current line
+    my @fields_line = split("\t", $current_line);
+	my @fields = split("_", $fields_line[0]);
+    
+    print $fields[4], "_", $fields[5], "\t", $fields_line[1], "\n";
+}
+close(ORF);
